d44d8ca42974b11f83aa20fe8e08676817ba8cf2
franciscolevim <franciscolevim@gmail.com>
2020-06-14 09:22:13 -0500

    Paquetes del modelo y de los servicios de la aplicación

---
+++ source/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='pokebook',
+    version='1.0',
+    description = '',
+    author = 'levbono',
+    author_email = 'franciscolevim@gmail.com',
+    url = 'https://github.com/franciscolevim/pokebook',
+    packages = ['models', 'services']
+)
