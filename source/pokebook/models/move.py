7439802a0d82351f8f0d93947c0f4b17a6123433
Francisco Leví <franciscolevim@gmail.com>
2020-06-18 17:26:09 -0500

    Ampliar la información que puede contener un movimiento.

--- source/pokebook/models/move.py
+++ source/pokebook/models/move.py
@@ -7,11 +7,14 @@ class Move(AbstractModel):
     """
     def __init__(self, name:str, url = ''):
         self.name = name
+        self.accuracy = 0
+        self.pp = 0
+        self.power = 0
         self.url = url
 
     
     def __str__(self):
-        return f'Move(name:{self.name})'
+        return f'Move(name:{self.name}, accuracy:{self.accuracy}, pp:{self.pp}, power:{self.power})'
 
 
     def __eq__(self, right):
@@ -30,3 +33,36 @@ class Move(AbstractModel):
         if not name or name.strip() == '':
             raise(ValueError('El valor del movimiento no puede ser None ni una cadena en blanco o vacía.'))
         self.__name = name.strip()
+
+    @property
+    def accuracy(self):
+        """
+        La precisión debe deser un valor entero positivo partiendo de cero.
+        """
+        return self.__accuracy
+
+    @accuracy.setter
+    def accuracy(self, accuracy:int):
+        self.__accuracy = accuracy if accuracy and accuracy >= 0 else 0
+
+    @property
+    def pp(self):
+        """
+        El PP debe deser un valor entero positivo partiendo de cero.
+        """
+        return self.__pp
+
+    @pp.setter
+    def pp(self, pp:int):
+        self.__pp = pp if pp and pp >=0 else 0    
+
+    @property
+    def power(self):
+        """
+        El poder del movimiento debe deser un valor entero positivo partiendo de cero.
+        """
+        return self.__power
+
+    @power.setter
+    def power(self, power:int):
+        self.__power = power if power and power >= 0 else 0
